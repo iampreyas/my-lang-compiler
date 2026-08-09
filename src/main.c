8e4d596f7e132818fbbac7692881509f6642074f
iampreyas <preyasparmar5713@gmail.com>
2026-08-09 19:25:40 +0530

    backup before file reading support

--- src/main.c
+++ src/main.c
@@ -3,6 +3,28 @@
 #include "../include/parser.h"
 #include "../include/lexer.h"
 #include "../include/eval.h"
+char* read_file=fopen(filename,"rb");
+if(!file)
+{
+    printf("[ERROR] File '%s' not founded\n",filename);
+    return NULL;
+}
+fseek(file,0,SEEK_END);
+long size=ftell(file);
+fseek(file,0,SEEK_SET);
+char *buffer=(char *)malloc(size+1)
+{
+    if(!buffer)
+    {
+        printf("[ERROR] Memory allocation failed\n");
+        fclose(file);
+        return NULL;
+    }
+    fread(buffer,1,size,file);
+    buffer[size]='\0';
+    fclose(file);
+    return buffer;
+}
 void print_ast(ASTNode *node,int level)
 {
     if(!node)
